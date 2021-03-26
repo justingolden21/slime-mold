04f06ddf62e83d2668e89aacfa38c853ea30e49c
RGB Studios <justingolden21@gmail.com>
2021-03-25 22:11:27 -0700

    setting up canvas

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,1 +1,10 @@
-window.onload = ()=> console.log('page loaded');
+const WIDTH = 500, HEIGHT = 500;
+
+window.onload = ()=> {
+
+	let canvas = document.getElementById('canvas');
+	let ctx = canvas.getContext('2d');
+
+	canvas.width = WIDTH;
+	canvas.height = HEIGHT;
+};
