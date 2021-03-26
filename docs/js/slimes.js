05555713b2202b73f547aad1d9c08b50c139dfba
RGB Studios <justingolden21@gmail.com>
2021-03-26 00:10:37 -0700

    slimes start angled towards center

--- docs/js/slimes.js
+++ docs/js/slimes.js
@@ -11,8 +11,23 @@ class Slime {
 function getSlime() {
 	let x = Math.random()*WIDTH;
 	let y = Math.random()*HEIGHT;
-	let angle = Math.random()*2*Math.PI;
+	// let angle = Math.random()*2*Math.PI;
+	let angle = angleTo(x, y, WIDTH/2, HEIGHT/2);
+
+	// let x = WIDTH/2;
+	// let y = HEIGHT/2;
+	// let angle = Math.random() * 2 * Math.PI;
+	// x += Math.cos(angle) * Math.random() * WIDTH;
+	// y += Math.sin(angle) * Math.random() * HEIGHT;
+	// angle += Math.PI;
+	// console.log(x, y, angle);
+
 	return new Slime(x, y, angle);
+}
+
+// https://math.stackexchange.com/q/707673/527418
+function angleTo(x1, y1, x2, y2) {
+	return Math.atan2(y2-y1,x2-x1);
 }
 
 function drawSlime(slime) {
